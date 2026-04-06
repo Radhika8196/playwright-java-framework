9f44d88d6a6f5fc00d7a63b598a8fcbec76b702e
Radhika8196 <manasvimannaru@Manasvis-MacBook-Air.local>
2026-04-06 03:33:35 -0700

    modified:   target/surefire-reports/emailable-report.html

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,31 +3,63 @@ package tests;
 import utils.ConfigReader;
 import pages.LoginPage;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 
 public class LoginTest extends BaseTest {
+	LoginPage loginPage ;
+	ConfigReader config ;
+	@BeforeMethod
+	public void beforeTest() {
+//		page = context.newPage();
+		loginPage = new LoginPage(page);
+		config = new ConfigReader();
+		page.navigate(config.get("baseUrl"));
+	}
 
-    @Test
-    public void verifyLogin()
-    {
-        LoginPage loginPage = new LoginPage(page);
+	@Test(priority=4)
+	public void verifyLogin()
+	{
 
-        ConfigReader config = new ConfigReader();
+		loginPage.login(config.get("username"), config.get("password"));
+		String expText = "logged";
+		String actText = page.locator("#flash").textContent();
+		System.out.println(actText);        
+		assertThat(page.locator("#flash")).containsText(expText);
 
-        loginPage.navigate(config.get("baseUrl"));
-        loginPage.login(config.get("username"), config.get("password"));
-        
+	}
 
-        String expText = "logged";
-        String actText = page.locator("#flash").textContent();
-        
 
-        System.out.println(actText);        
-        assertThat(page.locator("#flash")).containsText(expText);
-       
-          
-        }
+	@Test(priority=1)
+	public void invalidLogin_wrongPassword() {
+
+		loginPage.login(config.get("username"), "wrongPassword");
+		System.out.println(page.locator("#flash").textContent());
+		assertThat(page.locator("#flash")).containsText("Your password is invalid!");
+
+	}
+
+
+	@Test(priority=2)
+	public void invalidLogin_wrongUsername() {
+
+		loginPage.login("wrongUser", config.get("password"));
+		System.out.println(page.locator("#flash").textContent());
+		assertThat(page.locator("#flash")).containsText("invalid!");
+	}
+
+
+	@Test(priority=3)
+	public void invalidLogin_emptyFields() {
+
+		page.click("#submit-login");
+		System.out.println(page.locator("#flash").textContent());
+		assertThat(page.locator("#flash")).containsText("Your username is invalid!");
+	}
+
+
 }
